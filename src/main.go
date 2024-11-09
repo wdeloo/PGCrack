ed5bffc9ad5d25ecc9c17a7b0814e68fdc2ad6ea
wDeloo <wdelo.hack@gmail.com>
2024-11-09 19:36:47 +0100

    deleted useless chars definition in incremental

--- src/main.go
+++ src/main.go
@@ -254,19 +254,12 @@ func main() {
 		wordlist.DoWordlistBruteForce(filePath, file, tryDecrypt, &count, *threads)
 
 	case "incremental":
-		chars := []rune{
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-			'.', '-', '_',
-		}
 		if *length != 0 {
 			*minLength = *length
 			*maxLength = *length
 		}
 
 		incremental.DoIncrementalBruteForce(filePath, chars, *minLength, *maxLength, &count, *threads, tryDecrypt)
-
 	}
 
 	printStatus()
